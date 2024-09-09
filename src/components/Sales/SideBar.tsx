3f5b2e6fc9325d30396bbc594d18b21afefdfe89
Flacko <lesliejoe2k2@gmail.com>
2024-09-09 18:33:55 +0000

    feat: dropdown

--- src/components/Sales/SideBar.tsx
+++ src/components/Sales/SideBar.tsx
@@ -1,3 +1,25 @@
+import { useState } from "react";
+import { BsArrowDown, BsArrowUp } from "react-icons/bs";
+
 export default function SideBar() {
-  return <div>SideBar</div>;
+  const [isOpen, setisOpen] = useState(false);
+
+  const showmenu = () => {
+    setisOpen(!isOpen);
+  };
+
+  return (
+    <aside>
+      <div className="mb-2 border-b border-b-[#C9D2DF]">
+        <h3 className="font-[inter] font-bold text-[32px]">Category</h3>
+      </div>
+      <div>
+        <div onClick={showmenu} className="flex gap-0.5 items-center">
+          <h5 className="tracking-tight font-[inter] font-normal">Coke</h5>
+          {isOpen ? <BsArrowDown /> : <BsArrowUp />}
+        </div>
+        {isOpen && <ul className="list-none">li</ul>}
+      </div>
+    </aside>
+  );
 }
