398c4e125be1380293ab13d032d59c95d3fbcfba
Flacko <lesliejoe2k2@gmail.com>
2024-09-11 23:05:35 +0000

    fixed products styling bug

--- src/components/Sales/Products.tsx
+++ src/components/Sales/Products.tsx
@@ -8,13 +8,13 @@ Button;
 export default function Products() {
   return (
     <div className="grid grid-cols-4">
-      <div className="flex flex-col gap-2">
+      <div className="flex flex-col gap-3">
         <img
           src={sales1}
           className="rounded-md w-[230px] h-[250px]"
           alt="cake"
         />
-        <div className="flex flex-xol gap-1">
+        <div className="flex flex-col gap-1">
           <h4 className="text-[#3084A9] font-medium text-[16px] ">
             Lady M Confection
           </h4>
@@ -37,7 +37,7 @@ export default function Products() {
           className="rounded-md w-[230px] h-[250px]"
           alt="cake"
         />
-        <div className="flex flex-xol gap-1">
+        <div className="flex flex-col gap-1">
           <h4 className="text-[#3084A9] font-medium text-[16px] ">
             Lady M Confection
           </h4>
@@ -45,6 +45,13 @@ export default function Products() {
             Strawberry Swirl Mile Crepe Cake
           </p>
           <span className="text-[#1F252C] fonr-normal text-16px">¢600.00</span>
+        </div>
+        <div className="flex items-center justify-between">
+          <div className="flex gap-0 5 items-center">
+            <span>4.6</span>
+            <Star />
+          </div>
+          <Button className={"m-0.5"} />
         </div>
       </div>
       <div className="flex flex-col gap-2">
@@ -53,14 +60,21 @@ export default function Products() {
           className="rounded-md w-[230px] h-[250px]"
           alt="cake"
         />
-        <div className="flex flex-xol gap-1">
-          <h4 className="text-[#3084A9] font-medium text-[16px] ">
+        <div className="flex flex-col gap-1">
+          <h4 className="text-[#3084A9] font-medium">
             Lady M Confection
           </h4>
-          <p className="text-[#1F252C] fonr-normal text-16px">
+          <p className="text-[#1F252C] fonr-normal">
             Strawberry Swirl Mile Crepe Cake
           </p>
           <span className="text-[#1F252C] fonr-normal text-16px">¢600.00</span>
+        </div>
+        <div className="flex items-center justify-between">
+          <div className="flex gap-0 5 items-center">
+            <span>4.6</span>
+            <Star />
+          </div>
+          <Button className={"m-0.5"} />
         </div>
       </div>
       <div className="flex flex-col gap-2">
@@ -69,7 +83,7 @@ export default function Products() {
           className="rounded-md w-[230px] h-[250px]"
           alt="cake"
         />
-        <div className="flex flex-xol gap-1">
+        <div className="flex flex-col gap-1">
           <h4 className="text-[#3084A9] font-medium text-[16px] ">
             Lady M Confection
           </h4>
@@ -77,6 +91,13 @@ export default function Products() {
             Strawberry Swirl Mile Crepe Cake
           </p>
           <span className="text-[#1F252C] fonr-normal text-16px">¢600.00</span>
+        </div>
+        <div className="flex items-center justify-between">
+          <div className="flex gap-0 5 items-center">
+            <span>4.6</span>
+            <Star />
+          </div>
+          <Button className={"m-0.5"} />
         </div>
       </div>
     </div>
--- src/pages/FlashSales.tsx
+++ src/pages/FlashSales.tsx
@@ -1,6 +1,7 @@
 import Contact from "../components/Contact";
 import CTA from "../components/Sales/CTA";
 import Header from "../components/Sales/Header";
+import Products from "../components/Sales/Products";
 import SideBar from "../components/Sales/SideBar";
 
 export default function FlashSales() {
@@ -10,7 +11,9 @@ export default function FlashSales() {
       <Header />
       <div className="w-[95%] my-0 mx-auto grid grid-cols-[25%,75%]">
         <SideBar />
-        <div className="mt-2 text-[24px]">goods!</div>
+        <div className="mt-2 text-[24px]">
+          <Products />
+        </div>
       </div>
       <div className="my-4">
         <Contact />
