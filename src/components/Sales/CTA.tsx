53dec67189218ce63969223c8d264a2998d9435d
Flacko <lesliejoe2k2@gmail.com>
2024-10-08 18:31:18 +0000

    fixes

--- src/components/Sales/CTA.tsx
+++ src/components/Sales/CTA.tsx
@@ -1,12 +1,19 @@
-import salesCta from "../../images/salesCta.png";
+import bestCake from "../../images/bestCakes.jpg";
 
 export default function CTA() {
   return (
     <>
       <div
-        className="bg-cover bg-center h-[561px]"
-        style={{ backgroundImage: `url(${salesCta})`}}
-      ></div>
+        className="bg-cover bg-center h-auto md:h-[501px] relative"
+        style={{
+          backgroundImage: `linear-gradient(to bottom, rgba(145, 183, 226, 0.8), rgba(223, 177, 227, 0.8)), url(${bestCake})`,
+        }}
+      >
+        <div className="grid place-content-center items-center h-screen">
+          <h1 className="text-[#FFFFFF] font-medium md:text-[64px] text-center">BEST-SELLING CAKES</h1>
+        </div>
+      </div>
     </>
   );
 }
+F
