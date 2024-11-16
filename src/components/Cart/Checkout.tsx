252ed7ea6af645c907b8aaf127ee4711846aafb9
Flacko <lesliejoe2k2@gmail.com>
2024-11-16 14:56:33 +0000

    feat: checkout page

--- src/components/Cart/Checkout.tsx
+++ src/components/Cart/Checkout.tsx
@@ -1,32 +1,45 @@
 function Checkout() {
     return (
-        <div className="w-[85%] my-0 mx-auto">
-            <div className="grid grid-cols-[80%_20%] shadow-md">
-                <div className="p-2 shadow-md rounded">
-                    <header className="my-2">
+        <div className="w-[94%] my-0 mx-auto">
+            <div className="grid grid-cols-[70%_30%] gap-4 my-6">
+                <div className="py-2 px-4 shadow-md rounded">
+                    <header className="my-8">
                         <h1 className="uppercase text-black font-bold text-lg tracking-tight">checkout</h1>
                     </header>
                     <div>
-                        <span className="uppercase tracking-wide font-bold text-base text-orange-300">billing details</span>
+                        <span className="uppercase tracking-wide font-bold text-base text-orange-300 my-4">billing details</span>
                     </div>
-                    <form action="" className="flex flex-col space-y-4 w-80 mx-auto">
-                        <div className="flex flex-col space-y-2">
-                            <label htmlFor="name" className="font-semibold">Name</label>
-                            <input
-                                type="text"
-                                id="name"
-                                placeholder="Enter your name"
-                                className="px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
-                            />
-                        </div>
-                        <div className="flex flex-col space-y-2">
-                            <label htmlFor="email" className="font-semibold">Email</label>
-                            <input
-                                type="email"
-                                id="email"
-                                placeholder="Enter your email"
-                                className="px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
-                            />
+                    <form action="/" className="mt-2">
+                        <div className="grid grid-cols-[1fr_1fr] gap-y-4 items-center">
+                            <div>
+                                <label htmlFor="name" className="block capitalize font-bold">name</label>
+                                <input
+                                    type="text"
+                                    id="name"
+                                    placeholder="Enter your name"
+                                    className="px-4 py-2 border border-gray-300 rounded-md 
+                                    focus:outline-none focus:border-blue-500 w-[400px]"
+                                />
+                            </div>
+                            <div>
+                                <label htmlFor="email" className="block capitalize font-bold">email</label>
+                                <input
+                                    type="email"
+                                    id="email"
+                                    placeholder="Enter your email"
+                                    className="px-4 py-2 border w-[400px] border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
+                                />
+                            </div>
+                            <div>
+                                <label htmlFor="phone" className="block capitalize font-bold">name</label>
+                                <input
+                                    type="number"
+                                    id="phone"
+                                    placeholder="Enter your phone"
+                                    className="px-4 py-2 border border-gray-300 rounded-md 
+                                    focus:outline-none focus:border-blue-500 w-[400px]"
+                                />
+                            </div>
                         </div>
                     </form>
                 </div>
